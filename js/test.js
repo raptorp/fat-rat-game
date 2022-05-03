fa186d87dc9f0951f1e3494c36790744e3eef5ac
raptorp <sabrinaotelia@hotmail.com>
2022-05-03 11:08:40 +0200

    object fall

---
+++ js/test.js
@@ -0,0 +1,10 @@
+// alert("hello mellow yellow") ; //
+console.log("hello mellow yellow") ;
+function sayHello(){
+    alert("hello") ;
+}
+document.querySelector("#cheese_sprite") .addEventListener("click", sayHello) ;
+
+function item_clicked(){
+    
+}
